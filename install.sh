89417c0b4cf191766e9b9d1fe4bbf9e8a7806405
Toby Daigle <toby.daigle@hotmail.ca>
2021-01-19 10:46:23 -0500

    Add install.sh script to install all available plugins

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e;
+
+KUBECTL_PATH="$(which kubectl)";
+KUBECTL_DIR="${KUBECTL_PATH%kubectl}"
+
+FILES=~/kronos/kubectl-plugins/kubectl-*.sh
+for LOCAL_FILEPATH in $FILES
+do
+    REAL_PATH=$(realpath $LOCAL_FILEPATH)
+    BASENAME="$(basename $LOCAL_FILEPATH)"
+    TOOL_NAME="${BASENAME%.sh}"
+    KUBECTL_FILE="$KUBECTL_DIR$TOOL_NAME"
+    ln -s -f $LOCAL_FILEPATH $KUBECTL_FILE
+    echo "Kubernetes plugin $TOOL_NAME installed."
+done
